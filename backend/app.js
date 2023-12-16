3588b65e569aa8a29581411b9830582a23818a6a
integgy <matthewregidor@gmail.com>
2023-12-16 05:27:58 -0800

    create csrf routes/api

--- backend/app.js
+++ backend/app.js
@@ -2,9 +2,13 @@ const express = require("express");
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
+const cors = require("cors")
+const csurf = require("csurf")
+const {isProduction} = require("./config/keys")
 
 const usersRouter = require('./routes/api/users'); // update the import file path
-const tweetsRouter = require("./routes/api/tweets")
+const tweetsRouter = require("./routes/api/tweets");
+const csrfRouter = require("./routes/api/csrf");
 
 const app = express();
 
@@ -13,9 +17,25 @@ app.use(express.json()); // parse JSON request body
 app.use(express.urlencoded({ extended: false })); // parse urlencoded request body
 app.use(cookieParser()); // parse cookies as an object on req.cookies
 
+if (!isProduction) {
+    app.use(cors())
+}
+
+
+app.use(
+    csurf({
+        cookie: {
+            secure: isProduction,
+            sameSite: isProduction && "LAX",
+            httpOnly: true
+        }
+    })
+);
+
 
 // Attach Express routers
 app.use('/api/users', usersRouter); // update the path
-app.use("/api/tweets")
+app.use("/api/tweets", tweetsRouter)
+app.use("/api/csrf", csrfRouter)
 
 module.exports = app;
