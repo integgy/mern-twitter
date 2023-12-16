c52fabc69c936f8b886724f24cc60ba5363d5636
integgy <matthewregidor@gmail.com>
2023-12-16 05:39:07 -0800

    create error handling res messages

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const debug = require("debug")
 
 const cors = require("cors")
 const csurf = require("csurf")
@@ -17,9 +18,21 @@ app.use(express.json()); // parse JSON request body
 app.use(express.urlencoded({ extended: false })); // parse urlencoded request body
 app.use(cookieParser()); // parse cookies as an object on req.cookies
 
+
+
 if (!isProduction) {
     app.use(cors())
 }
+
+
+app.use((req, res, next) => {
+    const err = new Error("Not Found");
+    err.statusCode = 404;
+    next(err);
+});
+
+const serverErrorLogger = debug("backend:error");
+
 
 
 app.use(
@@ -37,5 +50,15 @@ app.use(
 app.use('/api/users', usersRouter); // update the path
 app.use("/api/tweets", tweetsRouter)
 app.use("/api/csrf", csrfRouter)
+app.use((err, req, res, next) => {
+    serverErrorLogger(err);
+    const statusCode = err.statusCode || 500;
+    res.status(statusCode);
+    res.json({
+        message: err.message,
+        statusCode,
+        errors: err.errors
+    })
+})
 
 module.exports = app;
